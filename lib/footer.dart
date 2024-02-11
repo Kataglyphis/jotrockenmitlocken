e1d5694a7c6198430502a599e8ca119e041c3cc2
Jonas Heinle <jonasheinle@googlemail.com>
2024-02-11 23:39:17 +0100

    fixed bug in footer

--- lib/footer.dart
+++ lib/footer.dart
@@ -24,137 +24,135 @@ class _Footer extends State<Footer> {
     TextStyle textStyleFooter = const TextStyle(fontSize: 11);
     return SizedBox(
       height: 150.0,
-      child: Expanded(
-        child: Column(
-          children: [
-            const SizedBox(
-              height: 30,
-            ),
+      child: Column(
+        children: [
+          const SizedBox(
+            height: 30,
+          ),
 
-            // const Expanded(
-            //     child: Column(
-            //   mainAxisAlignment: MainAxisAlignment.end,
-            //   crossAxisAlignment: CrossAxisAlignment.end,
-            //   children: [],
-            // )),
-            // const Expanded(
-            //     child: Column(
-            //   mainAxisAlignment: MainAxisAlignment.end,
-            //   crossAxisAlignment: CrossAxisAlignment.end,
-            //   children: [],
-            // )),
+          // const Expanded(
+          //     child: Column(
+          //   mainAxisAlignment: MainAxisAlignment.end,
+          //   crossAxisAlignment: CrossAxisAlignment.end,
+          //   children: [],
+          // )),
+          // const Expanded(
+          //     child: Column(
+          //   mainAxisAlignment: MainAxisAlignment.end,
+          //   crossAxisAlignment: CrossAxisAlignment.end,
+          //   children: [],
+          // )),
 
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          TextButton(
-                            style: TextButton.styleFrom(
-                                textStyle: const TextStyle(
-                                    fontSize: 11, fontWeight: FontWeight.bold)),
-                            onPressed: () {
-                              if (widget.onSelectItem != null) {
-                                widget.onSelectItem!(
-                                    NonNavBarScreenSelected.imprint.value);
-                              }
-                            },
-                            child: Text(
-                              textAlign: TextAlign.center,
-                              AppLocalizations.of(context)!.imprint,
-                              style: const TextStyle(
-                                  fontWeight: FontWeight.bold, fontSize: 11),
-                            ),
-                          ),
-                          TextButton(
-                            onPressed: () {},
-                            child: Text(
-                              AppLocalizations.of(context)!.contact,
-                              style: TextStyle(
-                                  fontWeight: FontWeight.bold, fontSize: 11),
-                            ),
-                          ),
-                          TextButton(
-                            onPressed: () {},
-                            child: Text(
-                              AppLocalizations.of(context)!.privacyPolicy,
-                              style: TextStyle(
-                                  fontWeight: FontWeight.bold, fontSize: 11),
-                            ),
-                          ),
-                        ],
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          TextButton(
-                            onPressed: () {},
-                            child: Text(
-                              AppLocalizations.of(context)!.cookieStatement,
-                              style: TextStyle(
-                                  fontWeight: FontWeight.bold, fontSize: 11),
-                            ),
-                          ),
-                        ],
-                      ),
-                      Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            TextButton(
-                              onPressed: () {},
-                              child: Text(
-                                AppLocalizations.of(context)!
-                                    .declarationOnAccessibility,
-                                style: TextStyle(
-                                    fontWeight: FontWeight.bold, fontSize: 11),
-                              ),
-                            ),
-                          ])
-                    ]),
-                const SizedBox(
-                  width: 30,
-                ),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    SocialMediaWidgets(iconSize: 20),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        TextButton(
+                          style: TextButton.styleFrom(
+                              textStyle: const TextStyle(
+                                  fontSize: 11, fontWeight: FontWeight.bold)),
+                          onPressed: () {
+                            if (widget.onSelectItem != null) {
+                              widget.onSelectItem!(
+                                  NonNavBarScreenSelected.imprint.value);
+                            }
+                          },
+                          child: Text(
+                            textAlign: TextAlign.center,
+                            AppLocalizations.of(context)!.imprint,
+                            style: const TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 11),
+                          ),
+                        ),
+                        TextButton(
+                          onPressed: () {},
+                          child: Text(
+                            AppLocalizations.of(context)!.contact,
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 11),
+                          ),
+                        ),
+                        TextButton(
+                          onPressed: () {},
+                          child: Text(
+                            AppLocalizations.of(context)!.privacyPolicy,
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 11),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        TextButton(
+                          onPressed: () {},
+                          child: Text(
+                            AppLocalizations.of(context)!.cookieStatement,
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 11),
+                          ),
+                        ),
+                      ],
+                    ),
                     Row(
                         mainAxisAlignment: MainAxisAlignment.start,
                         crossAxisAlignment: CrossAxisAlignment.center,
                         children: [
-                          Text(
-                            AppLocalizations.of(context)!.disclaimer +
-                                "\n" +
-                                AppLocalizations.of(context)!.copyright,
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 11),
+                          TextButton(
+                            onPressed: () {},
+                            child: Text(
+                              AppLocalizations.of(context)!
+                                  .declarationOnAccessibility,
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 11),
+                            ),
                           ),
                         ])
-                  ],
-                ),
-              ],
-            ),
-            // const Expanded(
-            //     child: Column(
-            //   children: [],
-            // )),
+                  ]),
+              const SizedBox(
+                width: 30,
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SocialMediaWidgets(iconSize: 20),
+                  Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(
+                          AppLocalizations.of(context)!.disclaimer +
+                              "\n" +
+                              AppLocalizations.of(context)!.copyright,
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 11),
+                        ),
+                      ])
+                ],
+              ),
+            ],
+          ),
+          // const Expanded(
+          //     child: Column(
+          //   children: [],
+          // )),
 
-            // const Expanded(
-            //     child: Column(
-            //   children: [],
-            // )),
-          ],
-        ),
+          // const Expanded(
+          //     child: Column(
+          //   children: [],
+          // )),
+        ],
       ),
     );
   }
