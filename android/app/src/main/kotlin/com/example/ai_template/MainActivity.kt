84353a62312953dc24c1a1d71b413981971071c4
Jonas Heinle <jonasheinle@googlemail.com>
2024-02-27 08:34:37 +0100

    refactor orgie is out of CONTROLLLL

--- android/app/src/main/kotlin/com/example/ai_template/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.ai_template
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ jotrockenmitlocken/android/app/src/main/kotlin/com/example/jotrockenmitlocken/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.jotrockenmitlocken
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
