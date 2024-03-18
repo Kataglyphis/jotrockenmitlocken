e20e17e5b378cdafd421e33011c4a22c0451558a
Jonas Heinle <jonasheinle@googlemail.com>
2024-03-18 06:47:44 +0100

    changed false flag

--- jotrockenmitlocken/lib/Pages/Footer/Pages/configs/cookie_declaration_config.dart
+++ jotrockenmitlocken/lib/Pages/Footer/Pages/configs/cookie_declaration_config.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
 import 'package:jotrockenmitlockenrepo/Pages/Footer/footer_pages_creator.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
