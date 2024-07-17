125d78c3d5a6199189015d66d4911c6814e68a26
Jonas Heinle <jonas.heinle@ipa.fraunhofer.de>
2024-07-17 21:39:17 +0200

    trigger cicd

--- scripts/download_markdown_files.py
+++ scripts/download_markdown_files.py
@@ -19,7 +19,6 @@ def parse_args() -> argparse.Namespace:
 if __name__ == "__main__":
 
     args: argparse.Namespace = parse_args()
-
     webdevclient = WebDavClient(args.hostname, args.username, args.password)
     webdevclient.download_all_files_iterative(
         args.remote_base_path, args.local_base_path
