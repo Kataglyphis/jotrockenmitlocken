dd3d652de2adcb8661eb0221ee270f1414ad38cd
Jonas Heinle <jonas.heinle@ipa.fraunhofer.de>
2026-03-06 18:33:24 +0100

    fix

--- lib/Pages/Footer/Pages/open_source_licenses_page.dart
+++ lib/Pages/Footer/Pages/open_source_licenses_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:jotrockenmitlocken/l10n/app_localizations.dart';
 import 'package:jotrockenmitlockenrepo/Layout/ResponsiveDesign/single_page.dart';
